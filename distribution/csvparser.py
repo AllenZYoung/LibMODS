766177f27fc97799b73b068505f97e29d4071da6
AllenZYoung <allenzyoung97@gmail.com>
2018-12-22 17:43:42 -0500

    Modification for different categories

--- distribution/csvparser.py
+++ distribution/csvparser.py
@@ -1,30 +1,43 @@
 import csv
 from collections import namedtuple
 from pprint import pprint
-CSVFILE = '../2018-MidYearCommunityPartnershipPosters-PC-PM-LIC - Metadata.csv'
+CSVFILE = 'Posters.csv'
 
-def parse_row(row):
-  Record = namedtuple('Record', ['Author','Title','Keywords'])
+category_map_dict = {
+  'Poster': ['Author', 'Title', 'Keywords'],
+}
+
+def get_category_by_filename(filename):
+  if filename.startswith('Posters'):
+    return 'Poster'
+
+def parse_row(row, category):
+  fields = category_map_dict[category]
+  Record = namedtuple('Record', fields)
   r = None
   try:
-    r = Record(row[0],row[1],row[2])
+    print([one for one in row])
+    r = Record(*[one for one in row])
   except IndexError:
     print('illegal row here,\t{}'.format(row))
   return r
 
-def open_csv():
+def open_csv(filename=None):
+  if filename is None:
+    filename = CSVFILE
+  category = get_category_by_filename(filename)
   result = []
   linenumber = 0
-  with open(CSVFILE) as f:
+  with open(filename) as f:
     reader = csv.reader(f)
     for row in reader:
       if linenumber == 0:
         pass
       else:
-        result.append(parse_row(row))
+        result.append(parse_row(row, category))
       linenumber += 1
   pprint(result)
-  return result
+  return result, category
 
 def get_records():
   return open_csv()
--- distribution/xmlmaker.py
+++ distribution/xmlmaker.py
@@ -2,28 +2,30 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 
 SAMPLE_FILE = 'sample.xml'
-OUTPUT_FILE = '../output/OUTPUT-{}.xml'
+OUTPUT_FILE = '../output/Poster-{}.xml'
 from csvparser import *
 
 
-def output_one(record):
+def output_one(record, category):
   xml = None
   with open(SAMPLE_FILE) as f:
     xml = BeautifulSoup(f,'xml')
     print(xml)
 
-    title_node = xml.find('mods:title')
-    authorname_node = xml.find('mods:namePart')
-    modstopic_node = xml.find('mods:topic')
-    title_node.string = record.Title
-    authorname_node.string = record.Author
-    modstopic_node.string = record.Keywords
+    if category == 'Poster':
+      title_node = xml.find('mods:title')
+      authorname_node = xml.find('mods:namePart')
+      modstopic_node = xml.find('mods:topic')
+      title_node.string = record.Title
+      authorname_node.string = record.Author
+      modstopic_node.string = record.Keywords
 
   with open(OUTPUT_FILE.format(record.Title.replace(',','').replace(' ','').replace('/','')),'w') as f:
     f.write(str(xml))
 
 
 if __name__ == '__main__':
-  records = get_records()
+  records, category = get_records()
+
   for record in records:
-    output_one(record)
+    output_one(record, category)
